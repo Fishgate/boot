2e4d9e28cd7e019dff3757e51f34a0832f4ba6fb
Tyrone <ty@crayoncapsule.co.za>
2014-02-19 16:24:09 +0200

    Tyrone works a bit more on the home page with archives and its menu for uploading.. no working functionality yet just working on getting a good solid layout first, wil definitely needs kyles input on the functionality

--- home.php
+++ home.php
@@ -22,9 +22,107 @@
           </div>
       </nav>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+      
+      <!--========================================
+                        CONTAINER
+      =========================================-->
+      <div class="container" style="margin-top: 70px;">
+        <div class="row">
+            <div class="col-md-3">
+                <h2>Menu</h2>
+                <div class="bg-white">
+                    <h3>File selection</h3>
+                    <form method="POST" action="php/upload.php">
+                        <input type="file" name="upload" id="upload" placeholder="Upload" />
+                        <div class="br">&nbsp;</div>
+                        <button class="btn btn-primary full-width" type="button">Upload</button>
+                    </form>
+                    
+                    <hr />
+                    
+                    <h3>Status</h3>
+                    <div class="progress progress-striped active">
+                        <div class="progress-bar progress-bar-success"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+                          <span class="sr-only">45% Complete</span>
+                          45%
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-9">
+                <h2>Archives</h2>
+                
+                <div class="actions-holder">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                            Actions <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Delete</a></li>
+                            <li><a href="#">Share</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="bg-archive">
+                    <form>
+                        <div class="row archive-row">
+                            <div class="col-xs-1 text-center border-right"><input type="checkbox" /></div>
+                            <div class="col-xs-7">filetitlehere.pdf</div>
+                            <div class="col-xs-2">2014-02-19</div>
+                            <div class="col-xs-1 archiver-hold border-left">
+                                <a class="glyphicon glyphicon-share-alt archiver"></a>
+                            </div>
+                            <div class="col-xs-1 archiver-hold border-left">
+                                 <a class="glyphicon glyphicon-zoom-in archiver"></a>
+                            </div>
+                        </div>
+                        <!-- -->
+                        <div class="row archive-row">
+                            <div class="col-xs-1 text-center border-right"><input type="checkbox" /></div>
+                            <div class="col-xs-7">filetitlehere.pdf</div>
+                            <div class="col-xs-2">2014-02-19</div>
+                            <div class="col-xs-1 archiver-hold border-left">
+                                <a class="glyphicon glyphicon-share-alt archiver"></a>
+                            </div>
+                            <div class="col-xs-1 archiver-hold border-left">
+                                 <a class="glyphicon glyphicon-zoom-in archiver"></a>
+                            </div>
+                        </div>
+                        <!-- -->
+                        <div class="row archive-row">
+                            <div class="col-xs-1 text-center border-right"><input type="checkbox" /></div>
+                            <div class="col-xs-7">filetitlehere.pdf</div>
+                            <div class="col-xs-2">2014-02-19</div>
+                            <div class="col-xs-1 archiver-hold border-left">
+                                <a class="glyphicon glyphicon-share-alt archiver"></a>
+                            </div>
+                            <div class="col-xs-1 archiver-hold border-left">
+                                 <a class="glyphicon glyphicon-zoom-in archiver"></a>
+                            </div>
+                        </div>
+                        <!-- -->
+                        <div class="row archive-row">
+                            <div class="col-xs-1 text-center border-right"><input type="checkbox" /></div>
+                            <div class="col-xs-7">filetitlehere.pdf</div>
+                            <div class="col-xs-2">2014-02-19</div>
+                            <div class="col-xs-1 archiver-hold border-left">
+                                <a class="glyphicon glyphicon-share-alt archiver"></a>
+                            </div>
+                            <div class="col-xs-1 archiver-hold border-left">
+                                 <a class="glyphicon glyphicon-zoom-in archiver"></a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </div>
+      
+      
+      
+    <?php include 'includes/scripts.php' ?>
+    
   </body>
 </html>
